5798f40e7cfd2eb65f08ed018e0cea5578951767
BeatyPete <akira941@gmail.com>
2021-01-09 03:16:42 -0600

    12.4.6

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,15 @@
 /* This will drop/delete the tables every time you run the migration script, thus ensuring you're starting with a clean slate. */
 DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS voters;
+
+CREATE TABLE voters (
+  id INTEGER PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
 
 CREATE TABLE parties (
   id INTEGER PRIMARY KEY,
